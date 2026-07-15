2a6fa2b4c7e86a0ca7a6aa0272df3f834d43a1de
Parvane Fix <fix@parvane-salon.ir>
2026-07-15 20:38:57 +0000

    fix: حذف import غیرضروری HttpService از NotificationsService

--- backend/src/modules/notifications/notifications.service.ts
+++ backend/src/modules/notifications/notifications.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Notification, NotificationType } from './entities/notification.entity';
-import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
